2bef4dd25265f123dfdc0cc6dae18ea476ae52f3
horse-pixel <kachman4321@gmail.com>
2025-03-12 20:52:00 +0000

    implement authentication module with JWT strategy and user validation

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,24 @@
+import { IsEmail, IsString, MinLength, Matches } from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  email: string;
+
+  @IsString({ message: 'Password must be a string' })
+  @MinLength(8, {
+    message: 'Password is too short. It should be at least 8 characters long.',
+  })
+  @Matches(/(?=.*[A-Z])/, {
+    message: 'Password must contain at least one uppercase letter',
+  })
+  @Matches(/(?=.*[a-z])/, {
+    message: 'Password must contain at least one lowercase letter',
+  })
+  @Matches(/(?=.*[0-9])/, {
+    message: 'Password must contain at least one number',
+  })
+  @Matches(/(?=.*[!@#$%^&*])/, {
+    message: 'Password must contain at least one special character',
+  })
+  password: string;
+}
